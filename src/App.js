5c7bdfa37fa2dc7dfba4f38d9b351e4328b0bb4e
Axel Soler <axelss369@gmail.com>
2022-11-01 15:48:09 -0300

    conecting to Destiny api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
+const destinyCharacterURL = 'https://www.bungie.net/Platform/Destiny2/2/Profile/4611686018431622644/?components=200';
+const apiKey = '96e8ddde744f4a17bc7f2337f87cb563';
+
 function App() {
+  const [characters, setCharacters] = useState({});
+  useEffect(() => {
+    getUserCharacters()
+  },[]);
+  const getUserCharacters = async () => {
+    try {
+      const response = await axios({
+        method: 'GET',
+        baseURL: destinyCharacterURL,
+        headers: {
+          'X-API-Key': apiKey,
+      },
+    })
+      console.log(response);
+      setCharacters(response.data.Response.characters.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  if(characters.length === 0) {
+    return <h1>there are no characters</h1>
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Destiny2</h1>
+      {console.log(characters)}
+      {Object.keys(characters).map((character, i) => (
+          <li key={i}>{characters[character].characterId}</li>
+      ))
+      }
     </div>
   );
 }
