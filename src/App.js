9cba8041742a1747174553e3e0724480b078343d
Axel Soler <axelss369@gmail.com>
2022-11-14 19:42:29 -0300

    text align

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import axios from 'axios';
-import './App.css';
 
 const destinyCharacterURL = 'https://www.bungie.net/Platform/Destiny2/';
 const destinyUserURL = 'https://www.bungie.net/Platform/User/Search/Prefix/';
@@ -51,10 +50,6 @@ const App = () => {
     getUser(userName);
   };
 
-  if (characters.length === 0 && user.length === 0) {
-    return <h1>there are no characters</h1>;
-  }
-
   return (
     <div className="App">
       <h1>Destiny2</h1>
@@ -63,98 +58,108 @@ const App = () => {
         <input onChange={onChange} type="text" placeholder="Guardian Name" />
         <button type="submit">SEARCH</button>
       </form>
-      <h2>USER</h2>
-      <img src={`https://www.bungie.net${user.iconPath}`} alt="console" />
-      <p>
-        Bungie name:
-        {user.bungieGlobalDisplayName}
-      </p>
-      <p>
-        membership id:
-        {user.membershipId}
-      </p>
-      <p>
-        membershipType:
-        {user.membershipType}
-      </p>
-      <h2>CHARACTERS</h2>
-      <ul>
-        {Object.keys(characters).map((character) => (
-          <li key={characters[character].characterId}>
-            <div>
-              <img src={`https://www.bungie.net${characters[character].emblemBackgroundPath}`} alt="console" />
-              <p>
-                Class:
-                {' '}
-                { characters[character].classType === 0 && 'Titan' }
-                { characters[character].classType === 1 && 'Hunter' }
-                { characters[character].classType === 2 && 'Warlock' }
-              </p>
-              <p>
-                Race:
-                {' '}
-                { characters[character].raceType === 0 && 'Human' }
-                { characters[character].raceType === 1 && 'Awoken' }
-                { characters[character].raceType === 2 && 'Exo' }
-              </p>
-              <p>
-                Character light:
-                {' '}
-                {characters[character].light}
-              </p>
-              <p>
-                Character id:
-                {' '}
-                {characters[character].characterId}
-              </p>
-              <p>
-                Last played:
-                {' '}
-                {characters[character].dateLastPlayed}
-              </p>
-              <p>
-                Time played:
-                {' '}
-                {characters[character].minutesPlayedTotal}
-                {' '}
-                min
-              </p>
-              <ul>
-                <li>
-                  Movilidad:
-                  {' '}
-                  {characters[character].stats[2996146975]}
-                </li>
-                <li>
-                  Resistencia:
-                  {' '}
-                  {characters[character].stats[392767087]}
-                </li>
-                <li>
-                  Recuperacion:
-                  {' '}
-                  {characters[character].stats[1943323491]}
-                </li>
-                <li>
-                  Disciplina:
-                  {' '}
-                  {characters[character].stats[1735777505]}
-                </li>
-                <li>
-                  Intelecto:
-                  {' '}
-                  {characters[character].stats[144602215]}
-                </li>
-                <li>
-                  Fuerza:
-                  {' '}
-                  {characters[character].stats[4244567218]}
-                </li>
-              </ul>
-            </div>
-          </li>
-        ))}
-      </ul>
+      { Object.keys(user).length === 0 && <h2>There are no users</h2> }
+      { Object.keys(user).length !== 0 && (
+        <section id="user">
+          <h2>USER</h2>
+          <img src={`https://www.bungie.net${user.iconPath}`} alt="console" />
+          <p>
+            Bungie name:
+            {user.bungieGlobalDisplayName}
+          </p>
+          <p>
+            membership id:
+            {user.membershipId}
+          </p>
+          <p>
+            membershipType:
+            {user.membershipType}
+          </p>
+        </section>
+      )}
+      { Object.keys(characters).length === 0 && <h2>There are no characters</h2> }
+      { Object.keys(characters).length !== 0 && (
+        <section id="characters">
+          <h2>CHARACTERS</h2>
+          <ul>
+            {Object.keys(characters).map((character) => (
+              <li key={characters[character].characterId}>
+                <div>
+                  <img src={`https://www.bungie.net${characters[character].emblemBackgroundPath}`} alt="console" />
+                  <p>
+                    Class:
+                    {' '}
+                    { characters[character].classType === 0 && 'Titan' }
+                    { characters[character].classType === 1 && 'Hunter' }
+                    { characters[character].classType === 2 && 'Warlock' }
+                  </p>
+                  <p>
+                    Race:
+                    {' '}
+                    { characters[character].raceType === 0 && 'Human' }
+                    { characters[character].raceType === 1 && 'Awoken' }
+                    { characters[character].raceType === 2 && 'Exo' }
+                  </p>
+                  <p>
+                    Character light:
+                    {' '}
+                    {characters[character].light}
+                  </p>
+                  <p>
+                    Character id:
+                    {' '}
+                    {characters[character].characterId}
+                  </p>
+                  <p>
+                    Last played:
+                    {' '}
+                    {characters[character].dateLastPlayed}
+                  </p>
+                  <p>
+                    Time played:
+                    {' '}
+                    {characters[character].minutesPlayedTotal}
+                    {' '}
+                    min
+                  </p>
+                  <ul>
+                    <li>
+                      Movilidad:
+                      {' '}
+                      {characters[character].stats[2996146975]}
+                    </li>
+                    <li>
+                      Resistencia:
+                      {' '}
+                      {characters[character].stats[392767087]}
+                    </li>
+                    <li>
+                      Recuperacion:
+                      {' '}
+                      {characters[character].stats[1943323491]}
+                    </li>
+                    <li>
+                      Disciplina:
+                      {' '}
+                      {characters[character].stats[1735777505]}
+                    </li>
+                    <li>
+                      Intelecto:
+                      {' '}
+                      {characters[character].stats[144602215]}
+                    </li>
+                    <li>
+                      Fuerza:
+                      {' '}
+                      {characters[character].stats[4244567218]}
+                    </li>
+                  </ul>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </section>
+      )}
     </div>
   );
 };
