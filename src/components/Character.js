a33fc792adf35fbfdefe0cb01dfa6d2349b847e5
Axel Soler <axelss369@gmail.com>
2022-11-24 15:28:42 -0300

    new version of the stats, more changes to design coming

--- src/components/Character.js
+++ src/components/Character.js
@@ -3,76 +3,60 @@ import './Character.css';
 
 const Character = (props) => {
   const { guardian } = props;
-  const lastPlayed = new Date(guardian.dateLastPlayed).toString().slice(3).split('GMT')[0];
+  const lastPlayed = new Date(guardian.dateLastPlayed).toString().slice(3, 15);
   return (
-    <div>
-      <img className="emblem" src={`https://www.bungie.net${guardian.emblemBackgroundPath}`} alt="console" />
-      <p>
-        Class:
-        {' '}
-        { guardian.classType === 0 && 'Titan' }
-        { guardian.classType === 1 && 'Hunter' }
-        { guardian.classType === 2 && 'Warlock' }
-      </p>
-      <p>
-        Race:
-        {' '}
-        { guardian.raceType === 0 && 'Human' }
-        { guardian.raceType === 1 && 'Awoken' }
-        { guardian.raceType === 2 && 'Exo' }
-      </p>
-      <p>
-        Character light:
-        {' '}
-        {guardian.light}
-      </p>
-      <p>
-        Character id:
-        {' '}
-        {guardian.characterId}
-      </p>
-      <p>
-        Last played:
-        {' '}
-        {lastPlayed}
-      </p>
-      <p>
-        Time played:
-        {' '}
-        {guardian.minutesPlayedTotal}
-        {' '}
-        min
-      </p>
-      <ul>
-        <li>
-          Movilidad:
+    <div className="flex flex-col items-center">
+      <div className="flex text-xl mb-4">
+        <p className="text-success mr-2">
+          { guardian.raceType === 0 && 'Human' }
+          { guardian.raceType === 1 && 'Awoken' }
+          { guardian.raceType === 2 && 'Exo' }
           {' '}
-          {guardian.stats[2996146975]}
+          { guardian.classType === 0 && 'Titan' }
+          { guardian.classType === 1 && 'Hunter' }
+          { guardian.classType === 2 && 'Warlock' }
+        </p>
+        <p className="text-warning ml-2">
+          {guardian.light}
+        </p>
+      </div>
+      <img className="emblem mb-4" src={`https://www.bungie.net${guardian.emblemBackgroundPath}`} alt="console" />
+      <ul className="w-3/4">
+        <li className="flex justify-between">
+          <p>Movilidad:</p>
+          <p>{guardian.stats[2996146975]}</p>
         </li>
-        <li>
-          Resistencia:
-          {' '}
-          {guardian.stats[392767087]}
+        <li className="flex justify-between">
+          <p>Resistencia:</p>
+          <p>{guardian.stats[392767087]}</p>
         </li>
-        <li>
-          Recuperacion:
-          {' '}
-          {guardian.stats[1943323491]}
+        <li className="flex justify-between">
+          <p>Recuperacion::</p>
+          <p>{guardian.stats[1943323491]}</p>
         </li>
-        <li>
-          Disciplina:
-          {' '}
-          {guardian.stats[1735777505]}
+        <li className="flex justify-between">
+          <p>Disciplina:</p>
+          <p>{guardian.stats[1735777505]}</p>
         </li>
-        <li>
-          Intelecto:
-          {' '}
-          {guardian.stats[144602215]}
+        <li className="flex justify-between">
+          <p>Intelecto:</p>
+          <p>{guardian.stats[144602215]}</p>
         </li>
-        <li>
-          Fuerza:
-          {' '}
-          {guardian.stats[4244567218]}
+        <li className="flex justify-between">
+          <p>Fuerza:</p>
+          <p>{guardian.stats[4244567218]}</p>
+        </li>
+        <li className="flex justify-between">
+          <p>Total time:</p>
+          <p>
+            {guardian.minutesPlayedTotal}
+            {' '}
+            min
+          </p>
+        </li>
+        <li className="flex justify-between">
+          <p>Last played:</p>
+          <p>{lastPlayed}</p>
         </li>
       </ul>
     </div>
